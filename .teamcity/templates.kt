cc03f8a4c45907fccd77a4e350b22836bbc58159
Mark Vieira <portugee@gmail.com>
2020-02-22 01:23:50 -0800

    Templates need a name

--- .teamcity/templates.kt
+++ .teamcity/templates.kt
@@ -2,6 +2,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.Template
 
 object DefaultTemplate : Template({
+    name = "Default"
+
     vcs {
         root(DslContext.settingsRoot)
     }
